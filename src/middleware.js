2c9e78c6b3c419d524f67359731a31b022df7a9f
Shivam Thakkar <115493910+ShivamThakkar1211@users.noreply.github.com>
2025-02-28 15:57:04 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,16 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
+};
+
 export async function middleware(request) {
   const token = await getToken({ req: request });
-  console.log("Token in middleware:", token); // Debugging line
   const url = request.nextUrl;
 
-  const isAuthPage = url.pathname.startsWith("/sign-in") || 
-                     url.pathname.startsWith("/sign-up") || 
-                     url.pathname.startsWith("/verify");
+  // Prevent infinite redirects
+  const isAuthPage = url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up") || url.pathname.startsWith("/verify");
 
   if (token && isAuthPage) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
